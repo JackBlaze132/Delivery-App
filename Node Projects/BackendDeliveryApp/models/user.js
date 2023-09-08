e2d0dc40d6e0a3a1a2812c9f320c8af2e2f01e7f
David Martinez Cerquera <Zerquera@users.noreply.github.com>
2023-09-08 10:59:23 -0500

    diapositiva 24 a 48 - rutas para añadir valores a la base de datos

--- Node Projects/BackendDeliveryApp/models/user.js
+++ Node Projects/BackendDeliveryApp/models/user.js
@@ -7,4 +7,33 @@ User.getAll = () => {
     return db.manyOrNone(sql);
 }
 
+User.create = (user) => {
+
+    const sql = `
+    INSERT INTO
+        users(
+            email,
+            name,
+            lastname,
+            phone,
+            image,
+            password,
+            create_at,
+            updated_at
+        )
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNIG id
+    `;
+
+    return db.oneOrNone(sql, [
+        user.email,
+        user.name,
+        user.lasname,
+        user.phone,
+        user.image,
+        user.password,
+        new Date(),
+        new Date()
+    ]);
+}
+
 module.exports = User;
--- Node Projects/BackendDeliveryApp/routes/usersRoutes.js
+++ Node Projects/BackendDeliveryApp/routes/usersRoutes.js
@@ -4,4 +4,7 @@ module.exports = (app) => {
 
     //Traer datos
     app.get("/api/users/getAll", UsersController.getAll);
+
+    //Guardar datos
+    app.post('/api/users/create', UsersController.register);
 }
