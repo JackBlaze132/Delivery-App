0a65e30c81f9b2b610beba3170fb664dc7230f5b
ST2712 <111332821+ST2712@users.noreply.github.com>
2023-09-21 23:35:45 -0500

    Add: Fin sesion 7, jsonwebtoken y login por postman

--- Node Projects/BackendDeliveryApp/models/user.js
+++ Node Projects/BackendDeliveryApp/models/user.js
@@ -8,6 +8,43 @@ User.getAll = () => {
     return db.manyOrNone(sql);
 }
 
+User.findById = (id, callback) => {
+
+    const sql = `
+    SELECT
+    id,
+    email,
+    name,
+    lastname,
+    image,
+    phone,
+    password,
+    session_token
+    FROM
+        users
+    WHERE
+        id = $1`;
+    return db.oneOrNone(sql, id).then(user => {callback(null, user); })
+}
+
+User.findByEmail = (email) => {
+    const sql = `
+    SELECT
+        id,
+        email,
+        lastname,
+        image,
+        phone,
+        password,
+        session_token
+    FROM
+        users
+    WHERE
+        email = $1
+    `;
+    return db.oneOrNone(sql,email);
+};
+
 User.create = (user) => {
 
     const myPasswordHashed = crypto.createHash('md5').update(user.password).digest('hex');
@@ -38,6 +75,14 @@ User.create = (user) => {
         new Date(),
         new Date()
     ]);
+}
+
+User.isPasswordMatched = (userPassword, hash) => {
+    const myPasswordHashed = crypto.createHash('md5').update(userPassword).digest('hex');
+    if(myPasswordHashed === hash){
+        return true;
+    }
+    return false;
 }
 
 module.exports = User;
