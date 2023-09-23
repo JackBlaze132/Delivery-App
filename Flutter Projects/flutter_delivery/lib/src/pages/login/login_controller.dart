1c9f27559ac53357d5f835cda12aebbc06adf2f3
ST2712 <111332821+ST2712@users.noreply.github.com>
2023-09-22 20:25:00 -0500

    Add: Se agrega el login desde flutter

--- Flutter Projects/flutter_delivery/lib/src/pages/login/login_controller.dart
+++ Flutter Projects/flutter_delivery/lib/src/pages/login/login_controller.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/models/response_api.dart';
+import 'package:flutter_delivery/src/provider/user_provider.dart';
+import 'package:flutter_delivery/src/utils/my_snackbar.dart';
 
 class LoginController{
   BuildContext context;
   TextEditingController emailController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
 
-  Future init(BuildContext context){
+  UserProvider usersProvider = new UserProvider();
+
+  Future init(BuildContext context) async{
     this.context = context;
+    await usersProvider.init(context);
   }
 
   void  goToRegisterPage(){
     Navigator.pushNamed(context, 'register');
   }
 
-  void login(){
+  void login() async{
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
-    print('email: $email');
-    print('contraseña: $password');
+    ResponseApi responseApi = await usersProvider.login(email, password);
+    print('Respuesta objetc: ${responseApi}');
+    print('Respuesta: ${responseApi.toJson()}');
+    MySnackbar.show(context, responseApi.message);
 
   }
 }
