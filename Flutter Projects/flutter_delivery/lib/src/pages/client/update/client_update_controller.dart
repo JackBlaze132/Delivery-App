7a39437741cecac8ebeba4dfe0871cdb04a2482d
ST2712 <111332821+ST2712@users.noreply.github.com>
2023-10-16 17:49:29 -0500

    Add: Ahora en la pagina de actualizar perfil, los datos se traen automaticamente

--- Flutter Projects/flutter_delivery/lib/src/pages/client/update/client_update_controller.dart
+++ Flutter Projects/flutter_delivery/lib/src/pages/client/update/client_update_controller.dart
@@ -5,19 +5,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_delivery/src/models/response_api.dart';
 import 'package:flutter_delivery/src/provider/user_provider.dart';
 import 'package:flutter_delivery/src/utils/my_snackbar.dart';
+import 'package:flutter_delivery/src/utils/shared_pref.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sn_progress_dialog/sn_progress_dialog.dart';
 
 import '../../../models/user.dart';
 
 class ClientUpdateController{
+
   BuildContext context;
-  TextEditingController emailController = new TextEditingController();
+
   TextEditingController nameController = new TextEditingController();
   TextEditingController lastnameController = new TextEditingController();
   TextEditingController phoneController = new TextEditingController();
-  TextEditingController passwordController = new TextEditingController();
-  TextEditingController confirmPasswordController = new TextEditingController();
 
   UserProvider usersProvider = new UserProvider();
 
@@ -26,36 +26,34 @@ class ClientUpdateController{
   Function refresh;
   ProgressDialog _progressDialog;
   bool isEnable = true;
+  User user;
+  SharedPref _sharedPref = new SharedPref();
 
-  Future init(BuildContext context, Function refresh){
+  Future init(BuildContext context, Function refresh) async{
     this.context = context;
     this.refresh = refresh;
     usersProvider.init(context);
     _progressDialog = ProgressDialog(context: context);
+
+    user = User.fromJson(await _sharedPref.read('user'));
+
+    nameController.text = user.name;
+    lastnameController.text = user.lastname;
+    phoneController.text = user.phone;
+
+    refresh();
   }
 
   void register() async{
-    String email = emailController.text.trim();
     String name = nameController.text.trim();
     String lastname = lastnameController.text.trim();
     String phone = phoneController.text.trim();
-    String password = passwordController.text.trim();
-    String confirmPassword = confirmPasswordController.text.trim();
 
-    if(email.isEmpty || name.isEmpty || lastname.isEmpty || phone.isEmpty || password.isEmpty || confirmPassword.isEmpty){
+    if(name.isEmpty || lastname.isEmpty || phone.isEmpty){
       MySnackbar.show(context, "Debes ingresar todos los campos");
       return;
     }
 
-    if(confirmPassword != password){
-      MySnackbar.show(context, "Las contraseñas no coinciden");
-      return;
-    }
-
-    if(password.length < 6){
-      MySnackbar.show(context, "La contraseña debe tener al menos 6 caracteres");
-      return;
-    }
 
     /*if(imageFile == null){
       MySnackbar.show(context, 'Seleccione una imagen');
@@ -66,11 +64,9 @@ class ClientUpdateController{
     isEnable = false;
 
     User user = new User(
-        email: email,
         name: name,
         lastname: lastname,
         phone: phone,
-        password: password
     );
 
     Stream stream = await usersProvider.createWithImage(user, imageFile);
--- Flutter Projects/flutter_delivery/lib/src/pages/client/update/client_update_page.dart
+++ Flutter Projects/flutter_delivery/lib/src/pages/client/update/client_update_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_delivery/src/pages/client/update/client_update_controller.dart';
-import 'package:flutter_delivery/src/pages/register/register_controller.dart';
 import 'package:flutter_delivery/src/utils/custom_colors.dart';
 
 class ClientUpdatePage extends StatefulWidget {
@@ -53,6 +52,8 @@ class _ClientUpdatePageState extends State<ClientUpdatePage> {
       child: CircleAvatar(
         backgroundImage: _con.imageFile != null
             ? FileImage(_con.imageFile)
+            : _con.user?.image != null
+            ? NetworkImage(_con.user?.image)
             : AssetImage('assets/img/user_profile_2.png'),
         radius: 60,
         backgroundColor: Colors.grey[200],
@@ -60,43 +61,6 @@ class _ClientUpdatePageState extends State<ClientUpdatePage> {
     );
   }
 
-  Widget _circle(){
-    return Container(
-      width: 260,
-      height: 230,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: CustomColors.primaryColor
-      ),
-    );
-  }
-
-  Widget _textFieldEmail(){
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: CustomColors.primaryColorField
-      ),
-
-      child: TextField(
-        controller: _con.emailController,
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-          hintText: 'Correo electrónico',
-          border: InputBorder.none,
-          contentPadding: EdgeInsets.all(15),
-          hintStyle: TextStyle(
-              color: CustomColors.primaryColorDock
-          ),
-          prefixIcon: Icon(
-            Icons.email,
-            color: CustomColors.primaryColorDock,
-          ),
-        ),
-      ),
-    );
-  }
 
   Widget _textFieldName(){
     return Container(
@@ -173,57 +137,7 @@ class _ClientUpdatePageState extends State<ClientUpdatePage> {
     );
   }
 
-  Widget _textPassword(){
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: CustomColors.primaryColorField
-      ),
-      child: TextField(
-        controller: _con.passwordController,
-        obscureText: true,
-        decoration: InputDecoration(
-            hintText: 'Contraseña',
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.all(15),
-            hintStyle: TextStyle(
-                color: CustomColors.primaryColorDock
-            ),
-            prefixIcon: Icon(
-                Icons.lock,
-                color: CustomColors.primaryColorDock
-            )
-        ),
-      ),
-    );
-  }
 
-  Widget _textConfirmPassword(){
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: CustomColors.primaryColorField
-      ),
-      child: TextField(
-        controller: _con.confirmPasswordController,
-        obscureText: true,
-        decoration: InputDecoration(
-            hintText: 'Contraseña',
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.all(15),
-            hintStyle: TextStyle(
-                color: CustomColors.primaryColorDock
-            ),
-            prefixIcon: Icon(
-                Icons.lock_outline,
-                color: CustomColors.primaryColorDock
-            )
-        ),
-      ),
-    );
-  }
 
   Widget _buttonRegister(){
     return Container(
@@ -239,24 +153,6 @@ class _ClientUpdatePageState extends State<ClientUpdatePage> {
           padding: EdgeInsets.all(23),
         ),
         child: Text('Actualizar perfil'),
-      ),
-    );
-  }
-
-  Widget _backIcon(){
-    return IconButton(
-      onPressed: _con.goToLoginPage,
-      icon: Icon(Icons.arrow_back_ios_rounded, color: Colors.white,),
-    );
-  }
-  Widget _textRegister(){
-    return Text(
-      'REGISTRO',
-      style: TextStyle(
-          color: Colors.white,
-          fontFamily: 'NimbusSans',
-          fontSize: 22,
-          fontWeight: FontWeight.bold
       ),
     );
   }
